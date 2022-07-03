b288a05560a2c978258fba46b7c5ce508942cede
hellotzb <1328887499@qq.com>
2022-07-03 22:28:19 +0800

    feat: extend custom hooks
    --story=20220703

--- hooks/useAsync.js
+++ hooks/useAsync.js
@@ -31,3 +31,22 @@ export default function useAsync(callback, dependencies = []) {
 //     }, 1000);
 //   });
 // });
+
+// const DEFAULT_OPTIONS = {
+//   headers: { "Content-Type": "application/json" },
+// }
+
+// export default function useFetch(url, options = {}, dependencies = []) {
+//   return useAsync(() => {
+//     return fetch(url, { ...DEFAULT_OPTIONS, ...options }).then(res => {
+//       if (res.ok) return res.json()
+//       return res.json().then(json => Promise.reject(json))
+//     })
+//   }, dependencies)
+// }
+
+// const { loading, error, value } = useFetch(
+//   `https://jsonplaceholder.typicode.com/todos/${id}`,
+//   {},
+//   [id]
+// );
